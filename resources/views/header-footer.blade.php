7150ff6211f136d692125b1f6588a53e354e35f3
huyhoangepu997 <51711802+huyhoangepu997@users.noreply.github.com>
2019-08-06 16:26:15 +0700

    up

---
+++ resources/views/header-footer.blade.php
@@ -0,0 +1,135 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse my-menu" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Trang Chủ <span class="sr-only">(current)</span></a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Giới Thiệu
+         </a>
+         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Tin Tức
+       </a>
+       <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+      </div>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Quan Hệ Cổ Đông
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Tuyển Dụng</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Liên Hệ</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Sản Phẩm</a>
+    </li>
+  </ul>
+</div>
+</div>
+</nav>
+
+<!--  -->
+
+<div class="container">
+  <h2>Tập Đoàn Hợp Tác Cùng Chúng Tôi</h2>
+  <section class="customer-logos slider">
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/Picture1.png"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/đối tác/Ojitex-HaiPhong-_-Paper....png"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/Picture3.png"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/Picture4.jpg"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/đối tác/b.jpg"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/đối tác/photo-0-1507879905694.jpg"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/đối tác/viettinlogo.jpg"></a></div>
+    <div class="slide"><a href="#" title=""><img src="http://hhppaper.com/image/data/đối tác/tải xuống.jpg"></a></div>
+  </section>
+</div>
+
+
+<footer>
+  <div class="footer">
+    <div class="footer1">
+      <div class="container-fluid">
+        
+        <div class="container ">
+          <div class="row">
+            <div class="col-sm-4">
+              <h2>Bản Đồ</h2><p>
+               <div class="myframegmap"> 
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59578.13707239431!2d105.7874068063269!3d21.047343019143607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abf073bac499%3A0xff77503d19c06ae5!2zMjkgTmfDtSA1MyBOZ3V54buFbiBLaG_DoWksIELhuqFjaCDEkOG6sW5nLCBIYWkgQsOgIFRyxrBuZywgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1565057379292!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div></p><br>
+            </div>
+            <div class="col-sm-4">
+              <h2>Home</h2>
+              <p>
+                <i class="fa fa-home"></i>    <a href="#"> Home</a><br>
+                <i class="fa fa-user-o"></i>    <a href="#"> About Us</a><br>
+                <i class="fa fa-map-marker"></i>    <a href="#"> Contact Us</a><br>
+                <i class="fa fa-briefcase"></i>    <a href="#"> Services</a><br>
+                <i class="fa fa-question-circle"></i>    <a href="#"> Term & Conditions</a><br><br>
+
+
+              </p>
+
+
+            </div>
+            <div class="col-sm-4 ">
+              <h2>Liên Hệ</h2>
+              <p style="font-size: 90%" >
+               
+                <i class="fa fa-phone"></i>   <b>Điện Thoại</b> <a href="tel:0913.361.629"> (84-225) 397.9952   </a><br> <a href="tel:Fax: (84-225) 397.9951" title=""><b>Fax:</b> (84-225) 397.9951</a><br>
+                <i class="fa fa-envelope"></i>   <a href="hhppaper@gmail.com"> hhppaper@gmail.com</a><br>
+                <i class="fa fa-map-marker"></i>  <a href=""><b>Địa chỉ trụ sở chính:</b>   Số 194 đường Kiều Hạ, Phường Đông Hải 2, Quận Hải An, Thành phố Hải Phòng.</a><br>
+                <i class="fa fa-map-marker"></i>  <a href=""><b>Văn Phòng Đại Diện:</b>   Số 29 Ngõ 53 Nguyễn Khoái, Bạch Đằng, Hai Bà Trưng, Hà Nội</a> <br>
+                <i class="fa fa-home"></i>    <a href="#"> Website:  www.hhppaper.com </a><br>
+              </p>
+              
+              <br>
+            </div>
+          </div>
+          <div class="clear30"></div>
+        </div>
+      </div></div>
+
+
+      <div class="footer2">
+        <div class="container-fluid">
+          <div class="container">
+            <div class="row">
+              <div class="clear30"></div>
+              <div class="col-sm-12 text-center"><p><strong>copyright © 2013-2019 Công Ty cổ Phần Giấy Hoàng Hà Hải Phòng</strong></p></div>
+              <div class="clear30"></div>
+            </div>
+            
+          </div>
+          
+        </div>
+      </div>
+    </div>  
+
+  </footer>
---
+++ resources/views/master-layout.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sản phẩm</title>
+    <base href="{{asset('')}}">
+	<!-- boostrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- reset css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap&subset=vietnamese" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap" rel="stylesheet">
+    <!-- WOW JS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <!-- Slick css -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+    <!-- style.css -->
+    <link rel="stylesheet" href="{{ asset('css/header-footer.css') }}">
+
+</head>
+<body>
+@include('header')
+@yield('content')
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, facilis, expedita. Veniam alias numquam ducimus iure expedita cum voluptas nemo facilis impedit explicabo, earum ex temporibus error, libero. Quos, vero.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus doloribus nisi, est, tempora, architecto officiis ipsa itaque perspiciatis quam dolorem voluptas, animi numquam quaerat. Consectetur veritatis, quibusdam repellendus dolorem rerum.
+
+@include('footer')
+    
+</body>
+
+<!-- bootstrap -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- Font-awesome -->
+<script src="https://kit.fontawesome.com/df8fdd30a5.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<!-- Slick JS -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+<script src="js/jquery.exzoom.js"></script>
+<script src="js/slider.js"></script>
+<script src="js/main-product.js"></script>
+<script>
+    $(function(){
+        $("#exzoom").exzoom({
+            "autoPlay": false
+        });
+    });
+</script>
+<!-- WOW JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="{{ asset('js/footer-header.js') }}"></script>
+</html>
